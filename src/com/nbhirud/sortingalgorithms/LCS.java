a58d3f0f2745c965dc73a374a79391022cbbb11a
Nikhil Bhirud <nbhirud@gmail.com>
2017-04-11 18:50:02 -0400

    v1.5 added Least Common Subsequence code and readme files

---
+++ src/com/nbhirud/sortingalgorithms/LCS.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nbhirud.sortingalgorithms;
+
+/**
+ *
+ * @author nbhirud
+ */
+//LCS class which implements the LCS memorized algorithm 
+public class LCS {
+
+    private String s1 = new String();
+    private String s2 = new String();
+    private int[][][] c;
+    private int len1;
+    private int len2;
+    private int lcslength = 0;
+    private String lcsString = "";
+
+    public LCS(String s1, String s2) {
+        this.s1 = s1;
+        this.s2 = s2;
+        this.len1 = s1.length();
+        this.len2 = s2.length();
+        this.c = new int[len1 + 1][len2 + 1][2];
+        //this.b = new int[l1][l2]; 
+    }
+
+    public int[][][] getMatrix() {
+        return c;
+    }
+
+    public void printMatrix() {
+        System.out.println("c[][][0]");
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                System.out.print(c[i][j][0]);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+        System.out.println("c[][][1]");
+        for (int i = 0; i <= len1; i++) {
+            for (int j = 0; j <= len2; j++) {
+                System.out.print(c[i][j][1]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public void memoized_lcs() {
+        for (int i = 0; i <= len1; i++) {                //                  ……………………………(1) 
+            for (int j = 0; j <= len2; j++) {
+                c[i][j][0] = 0;
+                c[i][j][1] = 3;
+                //System.out.println("memoized_lcs"+i+", "+j); 
+                if ((i != 0) && (j != 0)) {
+                    lookup_lcs(i, j);
+                }
+            }
+        }
+        this.lcslength = c[len1][len2][0];
+        generateLCSString();
+    }
+
+    private void lookup_lcs(int i, int j) {
+        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+            c[i][j][0] = c[i - 1][j - 1][0] + 1;                    //          ……………………………(3)  	 
+            c[i][j][1] = 0; //diagonal arrow 
+        } else {
+            c[i][j][0] = c[i - 1][j - 1][0];     //………………………….(2)
+            c[i][j][1] = 3; //none 
+        }
+
+        if (c[i - 1][j][0] >= c[i][j][0]) {
+            c[i][j][0] = c[i - 1][j][0];
+            c[i][j][1] = 1; //up arrow                                     …………………………….(5)  
+        }
+
+        if (c[i][j - 1][0] >= c[i][j][0]) {
+            c[i][j][0] = c[i][j - 1][0];
+            c[i][j][1] = 2; //left arrow                                   ………………………………(4)  
+        }
+        //System.out.println("lookup_lcs"+i+", "+j); 
+    }
+
+    private void generateLCSString() {    //……………………………..(6)
+        int i = len1;
+        int j = len2;
+        while (i > 0 && j > 0) {
+            if (c[i][j][1] == 0) {
+                i--;
+                j--;
+                lcsString = s1.charAt(i) + lcsString;
+            } else if (c[i][j][1] == 1) {
+                i--;
+            } else if (c[i][j][1] == 2) {
+                j--;
+            }
+            //System.out.println("generate_lcs_string"+i+", "+j); 
+        }
+    }
+
+    public int getLCSLength() {
+        return lcslength;
+    }
+
+    public String getlcsString() {
+        return lcsString;
+    }
+
+}
---
+++ src/com/nbhirud/sortingalgorithms/LCSManager.java
@@ -0,0 +1,26 @@
+package com.nbhirud.sortingalgorithms;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nbhirud
+ */
+//Main Class which invokes the LCS memorized method and prints result. 
+public class LCSManager { 
+ 
+ 	public static void main(String[] args) { 
+ 	 	LCS newLCS = new LCS(args[0], args[1]); 
+ 	 	//LCS newLCS = new LCS("spanking","amputation"); 
+ 	 	newLCS.memoized_lcs(); 
+ 	 	 
+ 	 	System.out.println("The Length of LCS is: "+newLCS.getLCSLength()); 
+ 	 	System.out.println("The LCS string is: "+newLCS.getlcsString()); 
+ 	 	System.out.println("Below is the LCS matrix: ");  	 	newLCS.printMatrix(); 
+ 	} 
+ 
+} 

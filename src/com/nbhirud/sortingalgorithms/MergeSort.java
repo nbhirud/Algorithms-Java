3e6576066b5f7e61e0269a6256878454353f9345
Nikhil Bhirud <nbhirud@gmail.com>
2017-05-04 18:29:19 -0400

    merge sort v1

---
+++ src/com/nbhirud/sortingalgorithms/MergeSort.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nbhirud.sortingalgorithms;
+
+/**
+ *
+ * @author nbhirud
+ */
+
+//Data: 
+public class MergeSort {
+    public static void main(String[] args) {
+        //int[] data = {2,5,3,8,2,7,4,0,1,4,5,2,4,5,7,3};
+        int[] data = {1,3,2,5,4,9,8,7,6, 10};
+        MergeSorter m = new MergeSorter(data);
+        for(int i: m.sort()) {
+            System.out.print(i+ " ");
+        }
+    }
+}
+
+
---
+++ src/com/nbhirud/sortingalgorithms/MergeSorter.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nbhirud.sortingalgorithms;
+
+/**
+ *
+ * @author nbhirud
+ */
+public class MergeSorter {
+    int[] data;
+    MergeSorter(int[] d) {
+        data = d;
+    }
+    
+    public int[] sort() {
+        sorter(0, data.length -1); // 0 to 15 //16-1
+        return data;
+    }
+    private void sorter( int low, int high) {
+        System.out.println("sorter. low = "+low+", high = "+high);
+        if(low == high) {
+            return;
+        }
+        else { //if(j-i > 1){
+            int mid = (low+high)/2; //7
+            sorter(low, mid); // (0, 7)
+            sorter(mid + 1, high); //(8, 15)
+            merge(low, mid, high); //(0, 7, 15)
+        }
+    }
+    private void merge(int low, int mid, int high) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("sorter. low = "+low+", mid = "+mid+", high = "+high);
+        int[] n = new int[high-low];
+        
+        for(int i = low, j = mid+1, k = 0; (i<= mid)&&(j<=high);k++) {
+            //i = 0, j = 8; k = 0
+            if(data[i] >= data[j]) {
+                n[k] = data[j];
+                System.out.println("j incremented"+data[j]);
+                j++;
+                
+            }
+            else  {
+                n[k] = data[i];
+                System.out.println("i incremented"+data[i]);
+                i++;
+                
+            }
+        }
+        for(int i = 0; i < (high - low); i++) {
+            data[low+i] = n[i];
+        }
+    }
+}

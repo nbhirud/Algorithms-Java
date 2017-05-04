d2caada0670a89ef8cbaba8d949e21ee821360f4
Nikhil Bhirud <nbhirud@gmail.com>
2017-05-04 18:57:03 -0400

    merge sort v4

--- src/com/nbhirud/sortingalgorithms/MergeSorter.java
+++ src/com/nbhirud/sortingalgorithms/MergeSorter.java
@@ -39,7 +39,8 @@ public class MergeSorter {
         System.out.println("sorter. low = " + low + ", mid = " + mid + ", high = " + high);
         int[] n = new int[high - low];
         int i = low, j = mid + 1, k = 0;
-        while (k < (high - low)) {
+        //while (k < (high - low)) {
+        while ((i<=mid)&&(j<=high)) {
             //i = 0, j = 8; k = 0
             if ((j <= high) && (data[i] >= data[j])) {
                 n[k] = data[j];
